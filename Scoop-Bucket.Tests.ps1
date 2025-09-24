e5c790dbe5d90cc43bf23a7bd048d2c7510948de
Snowflyt <gaoge011022@163.com>
2025-09-24 15:00:50 +0800

    chore: Update configurations to latest Scoop bucket template

--- Scoop-Bucket.Tests.ps1
+++ Scoop-Bucket.Tests.ps1
@@ -1,2 +1,10 @@
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-. "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"
+
+# Ensure manifest schema validation enumerates only bucket/**/*.json
+$prevCI = $env:CI
+$env:CI = $null
+try {
+    . "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1" -BucketPath $PSScriptRoot
+} finally {
+    $env:CI = $prevCI
+}
